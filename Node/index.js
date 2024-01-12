a8f2cf72599ddcf0daf05414b51d06dfb672aa30
Aman Singh Parihar <amanparihar16@gmail.com>
2024-01-12 11:58:08 +0530

    Update index.js

--- Node/index.js
+++ Node/index.js
@@ -1,3 +1,4 @@
+// Example 1
 const user = require('./user');
 let name = user.refer();
 let id = user.id;
@@ -5,3 +6,9 @@ let world = user.name;
 console.log(id);
 console.log(name);
 console.log(world);
+
+//Example 2 destructuring
+const {Name, Location} = require('./user');
+const name = Name();
+const location = Location();
+console.log(`${name} and ${location}`);
