4a5ad60c9af920673319e2d4dfbdf823282172c8
Aman Singh Parihar <amanparihar16@gmail.com>
2024-01-12 11:58:41 +0530

    Update user.js

--- Node/user.js
+++ Node/user.js
@@ -1,3 +1,14 @@
+// Example 1
 const getName = () => 'Aman';
 module.exports = {id:2, name:'world',refer : getName};
 console.log(module.exports);
+
+// Example 2
+const getName = () => 'Aman';
+const getLocation =() => 'London';
+module.exports = 
+{   id:2, 
+    name:'world',
+    Name : getName,
+    Location: getLocation
+};
